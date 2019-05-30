797455ab17a98c03729b8a567518fbfae6db7c72
Davidmu1987 <37941491+Davidmu1987@users.noreply.github.com>
2019-05-29 18:56:15 -0600

    PrograAvanzada
    Tarea numero 1 de progra

---
+++ Tarea1/Ejemplo.php
@@ -0,0 +1,25 @@
+<form action="ejemplo.php" method="get">
+  <p>Campo Texto Plano: <input type="text" name="nombre" size="40"></p>
+<p>Esto es un ejemplo de una linea de texto1</p>
+<p>Esto es un ejemplo de una linea de texto2</p>
+<p>Esto es un ejemplo de una linea de texto3</p>
+<select name="menu">
+  <option>Uno</option>
+  <option>Dos</option>
+  <option>Tres</option>
+</select>
+<p>
+<input type="checkbox" name="casilla">Casilla 1
+<input type="checkbox" name="casilla">Casilla 2
+<input type="checkbox" name="casilla">Casilla 3
+</p>
+  <p>Sexo:
+    <input type="radio" name="hm" value="h"> Hombre
+    <input type="radio" name="hm" value="m"> Mujer
+  </p>
+
+<p>
+    <input type="submit" value="Enviar">
+    <input type="reset" value="Borrar">
+  </p>
+</form>
