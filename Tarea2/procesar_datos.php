b1834d37719ee78424c5cdd01f08708741d43e23
David <dmarinu87gmail.com>
2019-06-12 00:37:29 -0600

    Create procesar_datos.php
    archivo de php

---
+++ Tarea2/procesar_datos.php
@@ -0,0 +1,20 @@
+<?php
+//Nombre de usuario
+$nu = $_GET['name'];
+echo "Nombre de usuario: ".$nu;
+
+//Contraseña
+$c = $_GET['pass'];
+echo "Contraseña: ".$c;
+
+//Email
+$e = $_GET['email'];
+echo "Email: ".$e;
+
+//Fecha Nacimiento
+$fn = $_GET['cumpleanios'];
+echo "Fecha nacimiento: ".$fn;
+
+//Campo Oculto
+$co = $_GET['campodefault'];
+echo "Campo Oculto: ".$co;
